33d7ca12b96c9a4dddcef59f8764ee2f06678d09
Alex McKean <amckean@me.com>
2017-12-06 00:55:42 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
+#`Car` class will need access to the `Vehicle` class and will therefore need access to the file that contains that class
 require_relative "./vehicle.rb"
+
+#use the `<` to inherit the `Car` class from `Vehicle`
+class Car < Vehicle
+  
+end
