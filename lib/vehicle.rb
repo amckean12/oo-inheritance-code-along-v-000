453e2fd621f2d218a9c63dab88bcca500aa9aa9c
Alex McKean <amckean@me.com>
2017-12-06 00:53:39 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,15 @@
 class Vehicle
+#Instances of `Vehicle` initialize with a wheel size and number.
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
 
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
