9e0507c886433ab01faf5b56889b3fc53213b33f
Alex McKean <amckean@me.com>
2017-12-06 00:57:12 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
 #Instances of `Vehicle` initialize with a wheel size and number.
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
